24e09438e8d5f868416e37fecf5269ec1278396d
aureliodeboa <aurelio74123@gmail.com>
2022-08-15 16:14:36 -0300

    Adiciona classes triviais do sintatico

--- analisadorlexico/src/main/java/com/mycompany/analisadorlexico/Parser.java
+++ analisadorlexico/src/main/java/com/mycompany/analisadorlexico/Parser.java
@@ -1,7 +1,46 @@
 
 package com.mycompany.analisadorlexico;
+  
+import java.io.IOException;
 
 public class Parser {
+    private Token currentToken;
+    private Scanner  scan;
     
+    public void parse(String args) throws IOException{
+        
+        scan = new Scanner();
+        
+        currentToken = scan.scan();
+        
+        if (currentToken != null)
+        {
+            parseProgram();
+        }
+    }
+    private void accept(byte expectedToken) throws ParserException{
+        if (currentToken.kind == expectedToken){
+            acceptIt();
+            
+        }
+        else{
+            //sintaxError(expectedToken);
+            System.out.println("ERROR");
+        }
+        
+    }
+    
+    private void sintaxError(byte expectedToken) throws ParserException{
+        throw new ParserException(currentToken, Token.spellings[expectedToken], scan);
+    }
+    private void acceptIt(){
+        Token temp = scan.scan();
+        while(temp == null){
+            temp = scan.scan();
+            
+        }
+        currentToken = temp;
+        
+    }
     
 }
