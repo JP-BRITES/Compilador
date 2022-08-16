14e4005d36069327bae7894d5f3a2c702a2f0e49
aureliodeboa <aurelio74123@gmail.com>
2022-08-16 16:41:48 -0300

    10 parses

--- analisadorlexico/src/main/java/com/mycompany/analisadorlexico/Parser.java
+++ analisadorlexico/src/main/java/com/mycompany/analisadorlexico/Parser.java
@@ -42,5 +42,119 @@ public class Parser {
         currentToken = temp;
         
     }
+<<<<<<< Updated upstream
+=======
+    //PARTE DE JP
+    
+    private void parseAtribuicao()throws ParserException{
+        parseVariavel();
+        
+        accept(Token.BECOMES);
+        
+        parseExpressao();
+    }
+    
+    private void parseBooLit()throws ParserException {
+        switch (currentToken.kind)
+        {
+            case Token.TRUE:
+                acceptIt();
+                break;
+               
+            case Token.FALSE:
+                acceptIt();
+                break;
+        
+    }
+    }
+    
+    private void parseComando() throws ParserException{
+        switch(currentToken.kind)
+         {
+            case Token.IDENTIFIER:
+                parseAtribuicao();
+                break;
+            case Token.IF:
+                paseCondicional();
+                break;
+            case Token.BEGIN:
+                paseComandoComposto();
+                break;
+            case Token.WHILE:
+                parseIterativo():
+                break;
+         }
+    }
+    
+    private void parseComandoComposto() throws ParserException{
+        accept(Token.BEGIN);
+       
+        parseListaDeComandos();
+        
+        accept(Token.END);
+    }
+    
+    private void paseCondicional()throws ParserException{
+        accept(Token.IF);
+        
+        parseExpressao();
+        
+        accept(Token.THEN);
+        
+        parseComando();
+        switch(currentToken.kind)
+        {
+            case Token.ELSE:
+                acceptIt();
+                parseComando();
+                break;
+            case Token.EOT:
+                acceptIt();
+                break;
+                
+        }
+    }
+    
+    private void parseCorpo()throws ParserException{
+        parseDeclaracoes();
+        parseComandoComposto();
+    }
+    
+    private void parseDeclaracao()throws ParserException{
+        parseDeclaracaoDeVariavel();
+        
+    }
+    
+    private void parseDeclaracaoDeVariavel()throws ParserException{
+        accept(Token.VAR);
+        parseListaDeIds();
+        accept(Token.COLON);
+        parseTipo();
+        
+    }
+    
+    private void parseDeclaracoes() throws ParserException{
+        
+        while (currentToken.kind == Token.VAR)
+        {
+           parseDeclaracao();
+        }
+    }
+    
+    private void parseExpressao() throws ParserException {
+        if(currentToken.kind == Token.MENQI || 
+            currentToken.kind == Token.MAIQ || 
+            currentToken.kind == Token.MAIQI || 
+            currentToken.kind == Token.MENQI || 
+            currentToken.kind == Token.IGUA || 
+            currentToken.kind == Token.DIF )
+        {
+            //acceptIt();
+            parseOpad();
+            parseTermo();
+        }
+    }
+    
+>>>>>>> Stashed changes
     
 }
