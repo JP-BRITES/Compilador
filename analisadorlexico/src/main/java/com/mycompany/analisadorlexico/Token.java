a141bcf18537b687c3ca4edfee51db983b334288
JP-BRITES <74506182+JP-BRITES@users.noreply.github.com>
2022-08-17 12:14:55 -0300

    +10 parses

--- analisadorlexico/src/main/java/com/mycompany/analisadorlexico/Token.java
+++ analisadorlexico/src/main/java/com/mycompany/analisadorlexico/Token.java
@@ -16,12 +16,12 @@ public class Token {
             BECOMES = 12, ADD= 13, SUB = 14, OR = 15, MUL = 16, DIV =17, 
             AND=18, MENQ = 19, MAIQ = 20,MENQI = 21, MAIQI = 22, IGUA = 23, 
             DIF= 24, COMEN = 25, RPAREN = 26,LPAREN = 27, COLON = 28,
-            DOT=29, EOT = 30, ERROR = 31, SEMICOLON = 32, REAL = 33;
+            DOT=29, EOT = 30, ERROR = 31, SEMICOLON = 32, REAL = 33, VIRG = 34;
     
     public final static String[] spellings ={
         "<indentifier>","<integer-literal>","begin","do","end","true","false","else","if","then", "var", "while",":=","add","sub","or","mul",
         "div","and","menor que","maior que","menor igual","maior igual", "igual","diferente","comentario",")","(",":",".","<eot>","<error>",
-        ";","real"
+        ";","real",","
     };
           
     public byte kind;
